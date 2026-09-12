8fec89a6be4426bbd2bf51ffb8a1523ec61dd41e
Daniel Bradáč <danielbradac297@gmail.com>
2026-09-12 19:45:40 +0200

    jmeno,bydliste,vek,telefon,email

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
 void main() {
 
 
-    System.out.printf("O prázdnínách jsem pracoval. Chodil jsem taky ven s kamarády. Odpočíval a relaxoval. Sledoval filmy a seriály. Cestoval jsem.");
-
+    System.out.printf("O prázdnínách jsem pracoval. Chodil jsem taky ven s kamarády. Odpočíval a relaxoval. Sledoval filmy a seriály. Cestoval jsem. \n");
+    System.out.printf("Daniel Bradáč \n");
+    System.out.printf("Nivnice \n");
+    System.out.printf("17 let \n");
+    System.out.printf("+420 736 141 945 \n");
+    System.out.printf("daniel.bradac@email.cz \n");
     }
